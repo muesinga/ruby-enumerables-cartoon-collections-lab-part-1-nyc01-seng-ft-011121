edba1f508961ea62ea3b23778f38eb758e36ea2d
Aaron Muesing <muesing.music@gmail.com>
2021-01-03 23:02:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 
-    array.each_with_index do |item, index|
+    array.each_with_index do | item, index |
       index = 1
       puts index + "." + " " + item
       index += 1
