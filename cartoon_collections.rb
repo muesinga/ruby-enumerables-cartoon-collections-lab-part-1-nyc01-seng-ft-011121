75600e6fab64d4de7aaa577fe3243986db865138
Aaron Muesing <muesing.music@gmail.com>
2021-01-03 23:09:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   
   array.each do | item |
-    puts "Hello" + " " + item + "!"
+    p "Hello" + " " + item + "!"
   end
 end
 
@@ -14,6 +14,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
 
     array.each_with_index do | item, index |
-      print index + "." + " " + item
+     p "#{index}:#{item}" 
   end
 end
